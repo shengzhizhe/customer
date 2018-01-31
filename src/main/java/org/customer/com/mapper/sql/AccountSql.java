89f5c7469dcef67d609a98c5a81c80ca55bb0366
xuesemofa@126.com <xuesemofa@126.com>
2018-01-31 11:24:20 +0800

    首次导入

---
+++ src/main/java/org/customer/com/mapper/sql/AccountSql.java
@@ -0,0 +1,23 @@
+package org.customer.com.mapper.sql;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.StringJoiner;
+
+public class AccountSql {
+    private static Logger logger = LoggerFactory.getLogger(AccountSql.class);
+
+    public String select_page_type(String type,String account) {
+        StringJoiner sql = new StringJoiner("");
+        sql.add("select * from account_table a where 1=1");
+        if (type != null && !type.isEmpty()) {
+            sql.add(" and a.acctype=#{type}");
+        }
+        if (account != null && !account.isEmpty()) {
+            sql.add(" and a.account=#{account}");
+        }
+        logger.info("[sql]---[" + sql.toString() + "]");
+        return sql.toString();
+    }
+}
---
+++ src/main/java/org/customer/com/model/ADD.java
@@ -0,0 +1,4 @@
+package org.customer.com.model;
+
+public interface ADD {
+}
